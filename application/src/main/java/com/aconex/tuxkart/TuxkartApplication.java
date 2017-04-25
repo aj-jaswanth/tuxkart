baf9429bf37186e55a5bef2b42e6fe07621f782b
Sandeep Chittanuri <skumar@2061-skumar.local>
2017-04-25 16:26:16 +0530

    Revert "added DB"
    This reverts commit 65dcf365d9a92dbf5f32682286e4b1854ca10b2b.

--- application/src/main/java/com/aconex/tuxkart/TuxkartApplication.java
+++ application/src/main/java/com/aconex/tuxkart/TuxkartApplication.java
@@ -1,14 +1,12 @@
 package com.aconex.tuxkart;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TuxkartApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TuxkartApplication.class, args);
 	}
-
-
 }
